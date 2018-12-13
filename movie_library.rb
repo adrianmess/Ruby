444189580870155e4fed04aafb193f7fb19d0c31
Adrian Messado <adrian.messado@gmail.com>
2018-12-12 16:11:56 -0800

    add case statement with actions to take based on user response

--- movie_library.rb
+++ movie_library.rb
@@ -4,3 +4,16 @@ movies = {
 
 puts "What would you like to do? "
 choice = gets.chomp
+
+case choice
+  when "add"
+  	puts "Added!"
+  when "update"
+  	puts "Updated!"
+  when "display"
+	  puts "Movies!"
+  when "delete"
+  	puts "Deleted!"
+	else 
+  	puts "Error!"
+end
