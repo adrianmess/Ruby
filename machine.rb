28713f839a8286115c13bb51643bf16e6af01402
Adrian Messado <adrian.messado@gmail.com>
2018-12-14 15:03:28 -0800

    add create method with filename parameter in Computer class, inside create declare time variable set to Time.now, create files instance has with filename as key and time as value, send message to user with puts that tells user name of file created with assoc username and time created

--- machine.rb
+++ machine.rb
@@ -3,7 +3,13 @@ class Computer
   def initialize(username, password)
     @username = username
     @password = password
-    @files = []
+    @files = {}
     @@users[username] = password
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
   end
 end
